febdc3e45ffb2ab98d99ac8bc69c25e367f0b3c1
Thejb11 <batool.alahmari@hotmail.com>
2026-04-23 22:16:25 +0300

    Create payment.py

---
+++ payment.py
@@ -0,0 +1,5 @@
+def process_payment(card_number, amount):
+    query = "INSERT INTO payments VALUES ('" + card_number + "'," + amount + ")"
+    api_key = "sk-1234567890"
+    print("Processing: " + card_number)
+    return query
